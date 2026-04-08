a59baea03667d4eb801cf699c408d9b5dfa838a1
vanphuc <vanphuc17504@gmail.com>
2026-04-08 15:02:20 +0700

    Tích hợp pug

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
+//Thiết lập views
+app.set('views', path.join(__dirname,"views"))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("client/pages/home"), 
+  {pageTitle: "Trang chủ"}
+})
+
+app.get('/tours', (req, res) => {
+  res.render("client/pages/tour-list"),
+  {pageTitle: "Danh sách tour"}
 })
 
 app.listen(port, () => {
