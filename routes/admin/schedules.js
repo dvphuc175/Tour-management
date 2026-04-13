5167d746b9c519bb91b1e4653e4c1ad2e56ca94f
vanphuc <vanphuc17504@gmail.com>
2026-04-13 08:44:11 +0700

    set up các file tuần 3 P2

--- routes/admin/schedules.js
+++ routes/admin/schedules.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ScheduleController = require('../../controllers/ScheduleController');
 
 router.get('/:id/edit', (req, res) => res.send('Trang sửa lịch trình'));
 
